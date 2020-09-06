889ee2f75dfa1fc622544811c480ddd557f35682
Zachary Selk <zrselk@gmail.com>
2020-09-05 23:13:27 -0600

    Move ImGui to docking branch (#32)

--- Engine/src/Util/imGuiLayer.cpp
+++ Engine/src/Util/imGuiLayer.cpp
@@ -47,28 +47,31 @@ void ImGuiLayer::onAttach() {
   ImGuiIO& io = ImGui::GetIO();
   io.BackendFlags |= ImGuiBackendFlags_HasMouseCursors;
   io.BackendFlags |= ImGuiBackendFlags_HasSetMousePos;
-  io.KeyMap[ImGuiKey_Tab] = GLFW_KEY_TAB;
-  io.KeyMap[ImGuiKey_LeftArrow] = GLFW_KEY_LEFT;
-  io.KeyMap[ImGuiKey_RightArrow] = GLFW_KEY_RIGHT;
-  io.KeyMap[ImGuiKey_UpArrow] = GLFW_KEY_UP;
-  io.KeyMap[ImGuiKey_DownArrow] = GLFW_KEY_DOWN;
-  io.KeyMap[ImGuiKey_PageUp] = GLFW_KEY_PAGE_UP;
-  io.KeyMap[ImGuiKey_PageDown] = GLFW_KEY_PAGE_DOWN;
-  io.KeyMap[ImGuiKey_Home] = GLFW_KEY_HOME;
-  io.KeyMap[ImGuiKey_End] = GLFW_KEY_END;
-  io.KeyMap[ImGuiKey_Insert] = GLFW_KEY_INSERT;
-  io.KeyMap[ImGuiKey_Delete] = GLFW_KEY_DELETE;
-  io.KeyMap[ImGuiKey_Backspace] = GLFW_KEY_BACKSPACE;
-  io.KeyMap[ImGuiKey_Space] = GLFW_KEY_SPACE;
-  io.KeyMap[ImGuiKey_Enter] = GLFW_KEY_ENTER;
-  io.KeyMap[ImGuiKey_Escape] = GLFW_KEY_ESCAPE;
-  io.KeyMap[ImGuiKey_KeyPadEnter] = GLFW_KEY_KP_ENTER;
-  io.KeyMap[ImGuiKey_A] = GLFW_KEY_A;
-  io.KeyMap[ImGuiKey_C] = GLFW_KEY_C;
-  io.KeyMap[ImGuiKey_V] = GLFW_KEY_V;
-  io.KeyMap[ImGuiKey_X] = GLFW_KEY_X;
-  io.KeyMap[ImGuiKey_Y] = GLFW_KEY_Y;
-  io.KeyMap[ImGuiKey_Z] = GLFW_KEY_Z;
+  io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+  io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+  io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+  // io.KeyMap[ImGuiKey_Tab] = GLFW_KEY_TAB;
+  // io.KeyMap[ImGuiKey_LeftArrow] = GLFW_KEY_LEFT;
+  // io.KeyMap[ImGuiKey_RightArrow] = GLFW_KEY_RIGHT;
+  // io.KeyMap[ImGuiKey_UpArrow] = GLFW_KEY_UP;
+  // io.KeyMap[ImGuiKey_DownArrow] = GLFW_KEY_DOWN;
+  // io.KeyMap[ImGuiKey_PageUp] = GLFW_KEY_PAGE_UP;
+  // io.KeyMap[ImGuiKey_PageDown] = GLFW_KEY_PAGE_DOWN;
+  // io.KeyMap[ImGuiKey_Home] = GLFW_KEY_HOME;
+  // io.KeyMap[ImGuiKey_End] = GLFW_KEY_END;
+  // io.KeyMap[ImGuiKey_Insert] = GLFW_KEY_INSERT;
+  // io.KeyMap[ImGuiKey_Delete] = GLFW_KEY_DELETE;
+  // io.KeyMap[ImGuiKey_Backspace] = GLFW_KEY_BACKSPACE;
+  // io.KeyMap[ImGuiKey_Space] = GLFW_KEY_SPACE;
+  // io.KeyMap[ImGuiKey_Enter] = GLFW_KEY_ENTER;
+  // io.KeyMap[ImGuiKey_Escape] = GLFW_KEY_ESCAPE;
+  // io.KeyMap[ImGuiKey_KeyPadEnter] = GLFW_KEY_KP_ENTER;
+  // io.KeyMap[ImGuiKey_A] = GLFW_KEY_A;
+  // io.KeyMap[ImGuiKey_C] = GLFW_KEY_C;
+  // io.KeyMap[ImGuiKey_V] = GLFW_KEY_V;
+  // io.KeyMap[ImGuiKey_X] = GLFW_KEY_X;
+  // io.KeyMap[ImGuiKey_Y] = GLFW_KEY_Y;
+  // io.KeyMap[ImGuiKey_Z] = GLFW_KEY_Z;
 
   // TODO: Figure out a better way to select the glsl version.
 #if defined(TRUNDLE_OS_MACOS)
@@ -177,6 +180,11 @@ void ImGuiLayer::end() {
   // backgroundColor->w);
   glClear(GL_COLOR_BUFFER_BIT);
   ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+  GLFWwindow* backup_current_context = glfwGetCurrentContext();
+  ImGui::UpdatePlatformWindows();
+  ImGui::RenderPlatformWindowsDefault();
+  glfwMakeContextCurrent(backup_current_context);
 
   // glfwSwapBuffers(nativeWindow);
 }
